8a42da8e4a5a2dcee52df438b1196cadb1a003ed
Fabian Möller <fabianm88@gmail.com>
2018-11-02 13:16:13 +0100

    fstest: remove WinPath from fstest.Item

--- fs/operations/operations_test.go
+++ fs/operations/operations_test.go
@@ -1389,7 +1389,6 @@ func TestDirMove(t *testing.T) {
 
 	for i := range files {
 		files[i].Path = strings.Replace(files[i].Path, "A1/", "A2/", -1)
-		files[i].WinPath = ""
 	}
 
 	fstest.CheckListingWithPrecision(
@@ -1419,7 +1418,6 @@ func TestDirMove(t *testing.T) {
 
 	for i := range files {
 		files[i].Path = strings.Replace(files[i].Path, "A2/", "A3/", -1)
-		files[i].WinPath = ""
 	}
 
 	fstest.CheckListingWithPrecision(
